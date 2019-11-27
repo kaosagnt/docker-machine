4638cf0da09a890b337a621073b2d0a813fc8a92
Jacopo Andrea Giola <jacopo@giola.org>
2019-11-27 09:25:44 +0100

    Add support for GCP Shared VPC
    If the google-network variable is set to a full link, get the
    project from it and use it for all the network related operations.

--- drivers/google/compute_util.go
+++ drivers/google/compute_util.go
@@ -30,6 +30,7 @@ type ComputeUtil struct {
 	project           string
 	diskTypeURL       string
 	address           string
+	networkProject    string
 	network           string
 	subnetwork        string
 	preemptible       bool
@@ -68,6 +69,19 @@ func newComputeUtil(driver *Driver) (*ComputeUtil, error) {
 		return nil, err
 	}
 
+	var networkProject string
+	if strings.Contains(driver.Network, "/projects/") {
+		var splittedElements = strings.Split(driver.Network, "/")
+		for i, element := range splittedElements {
+			if element == "projects" {
+				networkProject = splittedElements[i+1]
+				break
+			}
+		}
+	} else {
+		networkProject = driver.Project
+	}
+
 	return &ComputeUtil{
 		zone:                    driver.Zone,
 		instanceName:            driver.MachineName,
@@ -75,6 +89,7 @@ func newComputeUtil(driver *Driver) (*ComputeUtil, error) {
 		project:                 driver.Project,
 		diskTypeURL:             driver.DiskType,
 		address:                 driver.Address,
+		networkProject:          networkProject,
 		network:                 driver.Network,
 		subnetwork:              driver.Subnetwork,
 		preemptible:             driver.Preemptible,
@@ -187,7 +202,8 @@ func (c *ComputeUtil) region() string {
 }
 
 func (c *ComputeUtil) firewallRule() (*raw.Firewall, error) {
-	return c.service.Firewalls.Get(c.project, firewallRule).Do()
+	log.Infof("Getting firewall rule in project %s", c.networkProject)
+	return c.service.Firewalls.Get(c.networkProject, firewallRule).Do()
 }
 
 func missingOpenedPorts(rule *raw.Firewall, ports []string) map[string][]string {
@@ -247,12 +263,18 @@ func (c *ComputeUtil) openFirewallPorts(d *Driver) error {
 
 	if rule == nil {
 		create = true
+		var net string
+		if strings.Contains(d.Network, "/networks/") {
+			net = d.Network
+		} else {
+			net = c.globalURL + "/networks/" + d.Network
+		}
 		rule = &raw.Firewall{
 			Name:         firewallRule,
 			Allowed:      []*raw.FirewallAllowed{},
 			SourceRanges: []string{"0.0.0.0/0"},
 			TargetTags:   []string{firewallTargetTag},
-			Network:      c.globalURL + "/networks/" + d.Network,
+			Network:      net,
 		}
 	}
 
@@ -274,9 +296,9 @@ func (c *ComputeUtil) openFirewallPorts(d *Driver) error {
 
 	var op *raw.Operation
 	if create {
-		op, err = c.service.Firewalls.Insert(c.project, rule).Do()
+		op, err = c.service.Firewalls.Insert(c.networkProject, rule).Do()
 	} else {
-		op, err = c.service.Firewalls.Update(c.project, firewallRule, rule).Do()
+		op, err = c.service.Firewalls.Update(c.networkProject, firewallRule, rule).Do()
 	}
 
 	if err != nil {
@@ -359,7 +381,7 @@ func (c *ComputeUtil) createInstance(d *Driver) error {
 	if strings.Contains(c.subnetwork, "/subnetworks/") {
 		instance.NetworkInterfaces[0].Subnetwork = c.subnetwork
 	} else if c.subnetwork != "" {
-		instance.NetworkInterfaces[0].Subnetwork = "projects/" + c.project + "/regions/" + c.region() + "/subnetworks/" + c.subnetwork
+		instance.NetworkInterfaces[0].Subnetwork = "projects/" + c.networkProject + "/regions/" + c.region() + "/subnetworks/" + c.subnetwork
 	}
 
 	if !c.useInternalIPOnly {
