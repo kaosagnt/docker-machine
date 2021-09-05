d0df976ca7b233fecd7ea9c92daabafdf6b8bfa6
Ian McWilliam <kaosagnt@gmail.com>
2021-09-05 10:27:41 +1000

    Bring in changes from https://github.com/machine-drivers/machine
    machine/libmachine/shell/shell_windows.go
    
    Correctly detect powershell when ssh to windows machine.
    https://github.com/machine-drivers/machine/commit/92784a7c2afbf2f8cecfd8f2d8922a979e3a2728#diff-57884f90bc6641d6328d9e69f1a8b32b3aa22f5826178a49baf28cd3d6141707
    
    fix Powershell in CMD being mis-detected
    https://github.com/machine-drivers/machine/commit/8b02959660c6f3478eb4517345e445e61ff3cc83#diff-57884f90bc6641d6328d9e69f1a8b32b3aa22f5826178a49baf28cd3d6141707
    
    add missing .exe to shell check
    https://github.com/machine-drivers/machine/commit/0c7b6eac5f454860cec8ca0f7dd8893b9da782ee#diff-57884f90bc6641d6328d9e69f1a8b32b3aa22f5826178a49baf28cd3d6141707

--- libmachine/shell/shell_windows.go
+++ libmachine/shell/shell_windows.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"syscall"
 	"unsafe"
@@ -51,7 +52,9 @@ func getNameAndItsPpid(pid int) (exefile string, parentid int, err error) {
 func Detect() (string, error) {
 	shell := os.Getenv("SHELL")
 
-	if shell == "" {
+	// if you spawn a Powershell instance from CMD, sometimes the SHELL environment variable still points to CMD in the Powershell instance
+	// so if SHELL is pointing to CMD, let's do extra work to get the correct shell
+	if shell == "" || filepath.Base(shell) == "cmd.exe" {
 		shell, shellppid, err := getNameAndItsPpid(os.Getppid())
 		if err != nil {
 			return "cmd", err // defaulting to cmd
@@ -78,6 +81,10 @@ func Detect() (string, error) {
 
 	if os.Getenv("__fish_bin_dir") != "" {
 		return "fish", nil
+	}
+
+	if runtime.GOOS == "windows" {
+		shell = strings.TrimSuffix(shell, filepath.Ext(shell))
 	}
 
 	return filepath.Base(shell), nil
