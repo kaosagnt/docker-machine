77a00281d17e456e03fb195bd2d7f93cbface236
Ian McWilliam <kaosagnt@gmail.com>
2021-10-05 11:18:47 +1100

    Bump t2d version for release. 0.16.2-gitlab.13-t2d.3.

--- version/version.go
+++ version/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	Version = "0.16.2-gitlab.13-t2d.2"
+	Version = "0.16.2-gitlab.13-t2d.3"
 
 	// GitCommit will be overwritten automatically by the build system
 	GitCommit = "HEAD"
