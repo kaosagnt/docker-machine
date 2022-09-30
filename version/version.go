29bf757373e75d6d597480be229adc58f9126b37
Ian McWilliam <kaosagnt@gmail.com>
2022-09-30 11:18:40 +1000

    Bump version 0.16.2-gitlab.18-t2d.4

--- version/version.go
+++ version/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	Version = "0.16.2-gitlab.18-t2d.3"
+	Version = "0.16.2-gitlab.18-t2d.4"
 
 	// GitCommit will be overwritten automatically by the build system
 	GitCommit = "HEAD"
