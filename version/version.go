652a6747d8bc334683bdce66c4914f8f84cc93e7
Ian McWilliam <kaosagnt@gmail.com>
2026-07-13 14:13:41 +1000

    Bump -t2d.27

--- version/version.go
+++ version/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	Version = "0.16.2-gitlab.51-t2d.26"
+	Version = "0.16.2-gitlab.51-t2d.27"
 
 	// GitCommit will be overwritten automatically by the build system
 	GitCommit = "HEAD"
